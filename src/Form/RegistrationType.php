1a93b1ef14f52624e451bfcc8e699afb239ab084
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2018-10-15 22:49:11 +0200

    HomePage Controller + Registration Controller

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -8,11 +8,16 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('username', TextType::class, [
+                'label' => 'Pseudo'
+            ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom'
             ])
@@ -20,9 +25,30 @@ class RegistrationType extends AbstractType
                 'label' => 'Nom'
             ])
             ->add('email', EmailType::class)
+
             ->add('password', PasswordType::class, [
                 'label' => 'Mot de passe'
             ])
+            ->add('promotion', ChoiceType::class, array(
+                'choices'  => array(
+                    'B1' => "b1",
+                    'B2' => "b2",
+                    'B3' => "b3",
+                    'M1' => "m1",
+                    'M2' => "m2",
+                ),
+            ))
+            ->add('training', ChoiceType::class, array(
+                'choices'  => array(
+                    'Audiovisuel' => "audiovisuel",
+                    'Ingesup' => "ingesup",
+                    'Manaa' => "Manaa",
+                    'Digital Business School' => "dbs",
+                    'Web Com et Graphic Design' => "wcgd",
+                    'Animation 3D Jeux Vidéos' => "a3djv",
+                    'Lim\'Art' => "limart",
+                ),
+            ))
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire"
             ])
