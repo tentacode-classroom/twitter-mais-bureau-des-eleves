e3d00b7277220a44843f76c8b58fdbcd467a3793
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2018-10-21 16:35:01 +0200

    AppInstall

--- src/Command/CommandInstall.php
+++ src/Command/CommandInstall.php
@@ -1,12 +1,14 @@
 <?php
+
+// src/Command/CreateUserCommand.php
 namespace App\Command;
+
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
+
 class CommandInstall extends Command
 {
     protected static $defaultName = 'app:install';
