08fa629ec460baaab44d6937bacfdee48ec1a17e
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2018-10-21 12:39:25 +0200

    user crud + Admin controller

---
+++ src/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('avatar')
+            ->add('promotion')
+            ->add('training')
+            ->add('isActive')
+            ->add('password')
+            ->add('email')
+            ->add('username')
+            ->add('tweet_nb')
+            ->add('BDE')
+            ->add('like_nb')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
